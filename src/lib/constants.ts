b572ac3b8d892e5402fd514f12b5a0d254a80843
Anoop <anoopss6111@gmail.com>
2026-07-13 23:15:29 +0530

    chore: update department name and developer credits in footer

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ export const FEST_DATE = new Date('2026-07-20T09:00:00');
 export const FEST_VENUE = 'SFGC Yelahanka Campus, Bengaluru';
 export const FEST_TAGLINE = 'Enter the Castle. Prove Your Realm.';
 export const COLLEGE_NAME = 'Seshadripuram First grade College';
-export const DEPT_NAME = 'Seshadripuram Institute of Management Studies';
+export const DEPT_NAME = 'School of Management Studies';
 export const CONTACT_EMAIL = 'aakriti@simsblr.ac.in';
 export const COLLEGE_WEBSITE = 'https://www.simsblr.ac.in';
 
